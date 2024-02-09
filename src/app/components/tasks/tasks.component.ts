445b161c634f35529d1d99cec7228122d9d12802
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2024-02-09 12:51:41 +0530

    delete task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -19,4 +19,7 @@ export class TasksComponent implements OnInit {
     }))
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter((t) => t.id !== task.id))
+  }
 }
