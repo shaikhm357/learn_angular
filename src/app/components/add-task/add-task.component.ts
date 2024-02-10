2b84f783ac7d6cedcb731139e5c82f24e0b4134d
Shaikh Mahboob <shaikhm357@gmail.com>
2024-02-10 16:17:19 +0530

    subject in angular

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,40 +1,47 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
 import { Task } from '../../Task';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
-  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
-  id : string
-  text: string
-  day: string
-  reminder: boolean = false
-  constructor() { }
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  id: string;
+  text: string;
+  day: string;
+  reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
+
+  ngOnInit(): void {}
 
   onSubmit() {
     if (!this.text) {
-      alert('Please add task')
-      return
+      alert('Please add task');
+      return;
     }
 
     const newTask = {
       text: this.text,
       day: this.day,
-      reminder: this.reminder
-    }
+      reminder: this.reminder,
+    };
 
-    // event 
-    this.onAddTask.emit(newTask)
+    // event
+    this.onAddTask.emit(newTask);
 
-    this.text = ''
-    this.day = ''
-    this.reminder = false
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
-
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   title = 'learn_angular';
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
+
+  ngOnInit(): void {}
 
   toggleAddTask(): void {
-    console.log("Toggled...");
+    console.log('Toggled...');
+    this.uiService.toggleAddTask();
   }
-
 }
